395bf3d770cdf71fb3a8ff2d5c4c06a2113b33f4
PratamaAgung <apratamamia@gmail.com>
2019-04-24 16:54:22 +0700

    initial push

---
+++ src/components/Map.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="map" id="map">
+  </div>
+</template>
+
+<script>
+import L from 'leaflet'
+export default {
+  name: 'Map',
+  data() {
+    return {
+      map: null,
+      tileLayer: null,
+      layers: [],
+    }
+  },
+  methods: {
+    initMap() {
+      this.map = L.map('map').setView([-6.911056, 107.636914], 13);
+      this.tileLayer = L.tileLayer(
+        'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
+        {
+          maxZoom: 20
+        }
+      );
+      this.tileLayer.addTo(this.map);
+    },
+    initLayers() {},
+  },
+  mounted() {
+    this.initMap();
+    this.initLayers();
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.map {
+  height: 600px;
+}
+</style>
